99cdd11212d84912b1bfc53c55285d8366a81aa6
Mr. W <wojciech@coders-mill.com>
2015-12-09 10:24:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='migrations4neo',
+    packages=['migrations4neo'],
+    version='0.1',
+    description='Easy neo4j migrations',
+    author='Wojciech Rola',
+    author_email='wojciechrola@wp.pl',
+    url='https://github.com/turkus/migrations4neo',
+    download_url='https://github.com/turkus/migrations4neo/tarball/0.1',
+    keywords=['neo4j', 'migrations'],
+    classifiers=[],
+)
